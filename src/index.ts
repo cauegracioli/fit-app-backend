999dfa1a822a52eefa2279aa69d20aeff65026a8
cauegracioli <palaciocau@gmail.com>
2023-03-24 20:03:36 -0300

    fix: configurado para receber as rotas

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,10 @@
-const express = require("express");
-import { PrismaClient } from "@prisma/client";
-import { Request, Response } from "express";
-const routes = express.Router();
-
-const prisma = new PrismaClient();
+import routes from "./routes/routes";
+import express from "express";
 
 const app = express();
 
 app.use(express.json());
-
-app.get("/", async (req: Request, res: Response) => {
-  res.send("Rota funcionando");
-});
+app.use(routes);
 
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
